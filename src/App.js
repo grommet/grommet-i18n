387ffc325afe7fcb4b583c1d81afb9e339e9fcc9
iankbovard <iankbovard@gmail.com>
2020-03-02 09:55:36 -0600

    initial setup and package installs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grommet, Box, Text } from "grommet";
+
+function App() {
+  return (
+    <Grommet className="App">
+      <Box>
+        <Text>Hello</Text>
+      </Box>
+    </Grommet>
+  );
+}
+
+export default App;
