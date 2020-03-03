0671c1d115aedeade8f0eacbdddc4cb83250604b
iankbovard <iankbovard@gmail.com>
2020-03-03 09:15:26 -0600

    using i18n example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from "react";
-import { Grommet, Box, Text } from "grommet";
+import React, { Component, Suspense } from "react";
+import { useTranslation, withTranslation, Trans } from "react-i18next";
+import { Grommet, grommet, Box, Button, Heading } from "grommet";
 
-function App() {
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t } = this.props;
+    return <Heading>{t("title")}</Heading>;
+  }
+}
+const Welcome = withTranslation()(LegacyWelcomeClass);
+
+// Component using the Trans component
+function MyComponent() {
   return (
-    <Grommet>
-      <Box>
-        <Text>Hello</Text>
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <Grommet theme={grommet}>
+      <Box align="center">
+        <Welcome />
+        <Button onClick={() => changeLanguage("de")} label="de" />
+        <Button onClick={() => changeLanguage("en")} label="en" />
+      </Box>
+      <Box align="center">
+        <MyComponent />
+        <Box>{t("description.part2")}</Box>
       </Box>
     </Grommet>
   );
 }
 
-export default App;
+// loading component for suspense fallback
+const Loader = () => (
+  <Box className="App">
+    <Box>loading...</Box>
+  </Box>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Page />
+    </Suspense>
+  );
+}
