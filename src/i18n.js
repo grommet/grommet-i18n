782311205c0f29b8c06217fa1e7a3b0819bbacac
iankbovard <iankbovard@gmail.com>
2020-03-24 17:09:48 -0500

    fixed issue with config file

--- src/i18n.js
+++ src/i18n.js
@@ -15,7 +15,6 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    lng: "en",
     fallbackLng: "en", // use en if detected lng is not available
 
     saveMissing: true, // send not translated keys to endpoint
