29067ce716f9c891413c9f8a865450a768e114f3
iankbovard <iankbovard@gmail.com>
2020-04-01 11:20:05 -0500

    added all translation files

---
+++ src/Select/index.js
@@ -0,0 +1,73 @@
+import React, { Suspense, useState } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  Box,
+  Button,
+  Heading,
+  Grommet,
+  grommet,
+  Paragraph,
+  Select
+} from "grommet";
+
+const SelectExample = () => {
+  const [value, setValue] = useState();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    setValue();
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <Grommet theme={grommet}>
+      <Box gap="large">
+        <Box align="center">
+          <Heading level="3" textAlign="center">
+            {t("title")}
+          </Heading>
+          <Paragraph>
+            This example uses I18n to translate the string value found in
+            Select's messages prop.
+          </Paragraph>
+          <Select
+            options={[
+              t("selection.option1"),
+              t("selection.option2"),
+              t("selection.option3")
+            ]}
+            messages={{ multiple: t("messages.multiple") }}
+            multiple
+            value={value}
+            onChange={({ value: nextValue }) => setValue(nextValue)}
+          />
+        </Box>
+        <Box gap="small" align="center">
+          <Paragraph>{t("description.part2")}</Paragraph>
+          <Button
+            onClick={() => changeLanguage("en")}
+            label={t("language.option1")}
+          />
+          <Button
+            onClick={() => changeLanguage("de")}
+            label={t("language.option2")}
+          />
+        </Box>
+      </Box>
+    </Grommet>
+  );
+};
+
+const Loader = () => (
+  <Box>
+    <Box>loading...</Box>
+  </Box>
+);
+
+export default () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <SelectExample />
+    </Suspense>
+  );
+};
